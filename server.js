2362debdfb937008158d8f21c00f21064b7c32ce
abinishifex <abinishifex@gmail.com>
2026-07-22 14:30:28 +0300

    Add server functionality

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const http =   require('http');
 
+const fs = require('fs')
+
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
-});
+    console.log(req.url, req.method);
 
+
+res.setHeader('Content-Type', 'text/html');
+
+// res.write('hello, ninja ');
+// res.end();
+
+fs.readFile('./view/index.html', (err, data) => {
+    if(err){
+        console.log(err);
+        res.end();
+    }else{
+       
+        res.end(data);
+    }
+    
+})
+
+
+});
 server.listen(3000, 'localhost', () => {
     console.log('listen for request on port  3000')
 });
